e74369e2d3cd97c44c796be1ec472e7b730e4cb0
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-03-03 17:19:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,23 +8,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
- spielberg_titles = []
- spielberg_movies = directors_database[0][:movies]
-  i = 0
-  while i < spielberg_movies.length do
-    spielberg_titles << spielberg_movies[i][:title]
-    i += 1
-  end
-  puts spielberg_titles
- end
+  movie_index = 0 
+  while movie_index < directors_database[0][:movies].length do 
+    puts directors_database[0][:movies][movie_index][:title]
+    movie_index += 1 
+  end 
+end
 
-
-#row_index = 0
-#while row_index < spice_rack.count do
- # element_index = 0
-  #while element_index < spice_rack[row_index].count do
-   # puts spice_rack[row_index][element_index]
-    #element_index += 1
-  #end
-  #row_index += 1
-# end #=> nil
